b023a102ea081dd521336c2f6958d725e2e53ca1
se4oev <p.karpenko26@gmail.com>
2024-12-15 19:43:48 +0300

    вернул RestaurantVotingApplication как было

--- src/main/java/ru/javaops/bootjava/RestaurantVotingApplication.java
+++ src/main/java/ru/javaops/bootjava/RestaurantVotingApplication.java
@@ -3,11 +3,10 @@ package ru.javaops.bootjava;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @AllArgsConstructor
-public class RestaurantVotingApplication extends SpringBootServletInitializer {
+public class RestaurantVotingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RestaurantVotingApplication.class, args);
