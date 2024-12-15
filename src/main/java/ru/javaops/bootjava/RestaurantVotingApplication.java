46ac925d71f8ea6ba282c15ba905631b12ed03a3
se4oev <p.karpenko26@gmail.com>
2024-12-15 14:18:46 +0300

    apply 1_01_user_with_lombok.patch

--- src/main/java/ru/javaops/bootjava/RestaurantVotingApplication.java
+++ src/main/java/ru/javaops/bootjava/RestaurantVotingApplication.java
@@ -3,10 +3,11 @@ package ru.javaops.bootjava;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @AllArgsConstructor
-public class RestaurantVotingApplication {
+public class RestaurantVotingApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(RestaurantVotingApplication.class, args);
